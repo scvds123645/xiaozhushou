1c4a380729a012ed43da404824e4ee4e4a5dd898
scvds123645 <270fe019@gotrak.org>
2025-11-24 11:52:28 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
+      <BrowserRouter basename="/test">
+        <Routes> 
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
