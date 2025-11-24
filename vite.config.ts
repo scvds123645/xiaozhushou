411db1aa9153528b3c532d3d5ebc74baebfbc0cc
scvds123645 <270fe019@gotrak.org>
2025-11-24 11:52:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/test/',
   server: {
     host: "::",
     port: 8080,
